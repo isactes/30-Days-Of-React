1e7ab45ac384b359559ef8928ad1f0498979b59c
isactes <fidel.renovato@gmail.com>
2022-11-02 23:25:33 -0600

    day-01 Exercise 3

---
+++ 01_Day_JavaScript_Refresher/Exercise/day-01.js
@@ -0,0 +1,15 @@
+// Declare an empty array;
+ const anyArray = []
+//  Declare an array with more than 5 number of elements
+const oneArray = [ 'isac', 12, 'fidel', 2, 'febrero', 1994];
+
+
+// Find the length of your array
+const findArray = oneArray.filter(item => item.length <= 7);
+console.log('anyArray', anyArray);
+console.log('oneArray', oneArray);
+
+// Get the first item, the middle item and the last item of the array
+
+
+
